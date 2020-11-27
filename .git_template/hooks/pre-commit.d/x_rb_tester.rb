a6dcd359e1caadefed718cda239892eb6f3533fe
masukomi <masukomi@masukomi.org>
2020-11-27 12:19:17 -0500

    [git] x_rb_tester now runs ruby -c on files

--- .git_template/hooks/pre-commit.d/x_rb_tester.rb
+++ .git_template/hooks/pre-commit.d/x_rb_tester.rb
@@ -6,18 +6,28 @@ tested_files = Set.new()
 # tracking tested files because there may be changes to
 # foo.rb and foo_spec.rb which would result in
 # foo_spec.rb being invoked twice.
+def get_spec_filename(file)
+  return file if file.end_with? '_spec.rb'
+  "spec/#{file.sub(/^app\//, '').sub(/\.rb$/, '_spec.rb')}"
+end
+
 files.each do | file |
   filename=file
-  if ! file.end_with? '_spec.rb'
-    filename="spec/#{file.sub(/^app\//, '').sub(/\.rb$/, '_spec.rb')}"
-  end
-  if File.exist?(filename) && ! tested_files.include?(filename)
-    tested_files.add filename
+  if file.end_with? '.rb'
+    valid_code = system("ruby -c #{filename}")
+    exit 2 unless valid_code
 
-    response = system("bundle exec rspec #{filename}")
-    if ! response
-      puts "spec failed: #{filename}"
-      exit 1
+    filename = get_spec_filename(filename)
+    if File.exist?(filename) && ! tested_files.include?(filename)
+      tested_files.add filename
+
+      # normally....
+      # response = system("bundle exec rspec #{filename}")
+      response = system("/usr/local/bin/docker-compose exec -T api-server bin/bundle exec rspec #{filename}")
+      if ! response
+        puts "spec failed: #{filename}"
+        exit 1
+      end
     end
   end
 end
