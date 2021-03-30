b60547283171776514df884f6de96c322b9082d4
Kay Rhodes <kay_rhodes@apple.com>
2021-03-30 17:08:43 -0400

    [bin][fish] updated fao & faom
    faom: find and open migration now makes sure you specified something
    fao: now iterates over results of the find and asks if you want to
        open each, because sometimes there would be multiple and
        you would get the wrong one showing up in vim (other in a buffer)
    
        also now you can just get the filename without opening

--- .config/fish/functions/faom.fish
+++ .config/fish/functions/faom.fish
@@ -1,4 +1,8 @@
 function faom --description "Find And Open Migration"
-	set FILE_NAME (echo $argv | sed -e 's/\([A-Z]\)/\_\1/g' -e 's/\_//' | tr '[:upper:]' '[:lower:]')
-	fao "*$FILE_NAME.rb"
+	if [ "$argv" != "" ]
+		set FILE_NAME (echo $argv | sed -e 's/\([A-Z]\)/\_\1/g' -e 's/\_//' | tr '[:upper:]' '[:lower:]')
+		fao "*$FILE_NAME.rb"
+	else
+		echo "please specify a migration class name"
+	end
 end
