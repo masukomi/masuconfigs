7ae00ede094fc4eddb08797ea24d64e69db36640
masukomi <masukomi@masukomi.org>
2022-02-16 15:49:40 -0500

    [fish] rsn (rspec (by) name) now uses fbn

--- .config/fish/functions/rsn.fish
+++ .config/fish/functions/rsn.fish
@@ -1,9 +1,11 @@
 # run spec named...
-function rsn --argument-names "filename" -d "finds by name and then runs with rspec"
-  set -l path (find . -name "$filename")
+function rsn --argument-names "filename" "path" -d "finds by name and then runs with rspec"
+  # can't figure out how to call fbn
+  # and have it work without changing fbn to use echo which kills line
+  # breaks...
+  set -l ex_path (fbn "$filename" "$path")
   if test $status -eq 0
-    echo "$path"
-    bundle exec rspec --format=documentation "$path"
+    bundle exec rspec --format=documentation "$ex_path"
   else
     echo "Not found: $filename"
   end
