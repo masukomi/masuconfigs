999a0d688c4e18bb17ac8617e00ecc95d319111a
masukomi <masukomi@masukomi.org>
2022-04-25 10:31:45 -0400

    [doom] added toggle-window-split function

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -375,9 +375,9 @@
 
 ;; org-roam stuff
 
-; (make-directory "~/Documents/org-roam")
-; (setq org-roam-directory (file-truename "~/Documents/org-roam"))
-; (org-roam-db-autosync-mode)
+;; (make-directory "~/Documents/org-roam")
+(setq org-roam-directory (file-truename "~/Documents/org-roam"))
+(org-roam-db-autosync-mode)
 ;; (setq org-roam-database-connector 'sqlite3)
 
 ; org-roam-ui stuff
@@ -496,17 +496,33 @@
 
 (add-hook 'after-save-hook #'raco-fmt)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; undo tree mode                                                         ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;turn on everywhere
-(global-undo-tree-mode 1)
-(defalias 'redo 'undo-tree-redo)
-;; ;; make ctrl-z undo
-;; (global-set-key (kbd "C-z") 'undo)
-;; ;; make ctrl-Z redo
-;; (global-set-key (kbd "C-S-z") 'redo)
-;; make command-z undo
-(global-set-key (kbd "s-z") 'undo)
-;; make command-Z redo
-(global-set-key (kbd "s-S-z") 'redo)
+
+;;;;;;;;;;;;;;;;
+;; Toggle Window Split by JeffDWork
+;; found here: https://www.emacswiki.org/emacs/ToggleWindowSplit
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
+(define-key ctl-x-4-map "t" 'toggle-window-split)
