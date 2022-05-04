884f6f2145560d219fdce192a31fb258067c06bc
masukomi <masukomi@masukomi.org>
2022-05-04 17:57:00 -0400

    [fish][docker] armageddon now more robust

--- .config/fish/functions/armageddon.fish
+++ .config/fish/functions/armageddon.fish
@@ -1,8 +1,33 @@
 function armageddon
 	remove_containers
+	prune_docker_network
+	kill_dangling_docker_images
+	kill_docker_volumes
+	kill_docker_images
+end
+
+function prune_docker_network
 	docker network prune -f
-	docker rmi -f (docker images --filter dangling=true -qa)
-	docker volume rm (docker volume ls --filter dangling=true -q)
-	docker rmi -f (docker images -qa)
+end
+
+function kill_dangling_docker_images
+	set current_images (docker images --filter dangling=true -qa)
+	if test "$current_images" != ""
+		docker rmi -f "$current_images"
+	end
+end
+
+function kill_docker_images
+	set current_images (docker images -qa)
+	if test "$current_images" != ""
+		docker rmi -f "$current_images"
+	end
+end
+
+function kill_docker_volumes
+	set volumes (docker volume ls --filter dangling=true -q)
+	if test "$volumes" != ""
+		docker volume rm $volumes
+	end
 end
 
--- .config/fish/functions/remove_containers.fish
+++ .config/fish/functions/remove_containers.fish
@@ -1,5 +1,18 @@
 function remove_containers
+	shut_down_docker_volumes
+
+	kill_docker_processes
+	kill_docker_processes
+end
+
+function kill_docker_processes
+	set processes (docker ps -aq)
+	if test "$processes" != ""
+		docker stop "$processes"
+	end
+end
+
+function shut_down_docker_volumes
 	docker-compose down --volumes 2> /dev/null
-	docker stop (docker ps -aq)
-	docker rm (docker ps -aq)
+
 end
