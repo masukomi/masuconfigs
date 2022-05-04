9e201560575208ef6aace26c7af84724f60bf723
masukomi <masukomi@masukomi.org>
2022-05-04 17:59:09 -0400

    [fish][docker] renamed function in armageddon

--- .config/fish/functions/armageddon.fish
+++ .config/fish/functions/armageddon.fish
@@ -2,7 +2,7 @@ function armageddon
 	remove_containers
 	prune_docker_network
 	kill_dangling_docker_images
-	kill_docker_volumes
+	kill_dangling_docker_volumes
 	kill_docker_images
 end
 
@@ -24,7 +24,7 @@ function kill_docker_images
 	end
 end
 
-function kill_docker_volumes
+function kill_dangling_docker_volumes
 	set volumes (docker volume ls --filter dangling=true -q)
 	if test "$volumes" != ""
 		docker volume rm $volumes
