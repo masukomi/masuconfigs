101ba76041a727ced49f02b98e87ae37b21c4d61
masukomi <masukomi@masukomi.org>
2022-05-20 15:40:52 -0400

    [fish] fbn now uses fd instead of find
    note that this results in different (normal)
    regexp patterns
    
    and prettier output. ;)

--- .config/fish/functions/fbn.fish
+++ .config/fish/functions/fbn.fish
@@ -1,8 +1,14 @@
-function fbn --argument-names 'filename' 'path' -d "find by name. Looks for a file by name under the current dir"
+function fbn --argument-names 'filename' 'path' -d "find by name. Looks for a file by name under the current dir using regexp to match"
+
+	# details on fd and what it takes can be found here
+	# https://github.com/sharkdp/fd#readme
+	#
+	# fd can be installed with homebrew
+	# brew install fd
 	if test "$path" = ""
-		find . -name "$filename"
+		fd "$filename"
 	else
-		find $path -name "$filename"
+		fd $path "$filename"
 	end
 end
 
