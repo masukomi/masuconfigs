10b4558c3498cacf61fd93f50e66922f53609601
masukomi <masukomi@masukomi.org>
2022-05-25 10:52:09 -0400

    [fish] added description to where command

--- .config/fish/functions/where.fish
+++ .config/fish/functions/where.fish
@@ -1,4 +1,5 @@
-function where --argument-names 'exe_or_function'
+function where --argument-names 'exe_or_function' \
+  --description "finds an executable or fish function with the supplied name"
 
   set exe_path (command -v $exe_or_function)
   if test $status -eq 0
