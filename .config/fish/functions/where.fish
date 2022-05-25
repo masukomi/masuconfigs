cf393a0ee6bc55e25cdb8de4d2ad86579afdac83
masukomi <masukomi@masukomi.org>
2022-05-25 16:11:49 -0400

    [fish] fixed bug in where
    wasn't providing a response if the function
    was defined in a file that didn't match the
    function name, which was half of the purpose
    of writing this thing. 🤦‍♀

--- .config/fish/functions/where.fish
+++ .config/fish/functions/where.fish
@@ -9,12 +9,9 @@ function where --argument-names 'exe_or_function' \
     set function_names (functions --names | sd ", " "\n" | grep "^$exe_or_function")
     for x in $function_names
       set function_path (functions -D $x)
-      string match -r "/$exe_or_function.fish\$" "$function_path" > /dev/null
-      if test $status -eq 0
-        echo $function_path
-        return 0
-      end
+      echo $function_path
     end
+    return 0
   end
   return 1
 end
