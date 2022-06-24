ca71ebc7bbabf3dee6be472f23a4c84a54bb2ffb
masukomi <masukomi@masukomi.org>
2022-06-24 11:20:37 -0400

    [fish] fbn now supports --exec
    passing in --exec='foo' will allow you to
    execute a command against the files that were found.
    
    'foo' in teh example above is a text string where
    every instance of {} will be replaced with the
    path to the found file

--- .config/fish/functions/fbn.fish
+++ .config/fish/functions/fbn.fish
@@ -1,3 +1,21 @@
+# fbn: find by name
+# Usage: fbn <filename-section> [path] [--exec="<command>"]
+#
+#   If path is not specified it will search in and under the current
+#   directory.
+#   If exec is specified, any instance of {} will be replaced with the
+#   filename.
+#
+#   example: fbn ".*.rb" --exec="echo foo: {}"
+#   produces
+#   FILE: foo.rb ---------------------------------
+#   foo: foo.rb
+#   FILE: bar.rb ---------------------------------
+#   bar: bar.rb
+#
+# Dependencies:
+# fd: https://github.com/sharkdp/fd
+# sd: https://github.com/chmln/sd
 function fbn --argument-names 'filename' 'path' -d "find by name. Looks for a file by name under the current dir using regexp to match"
 
 	# details on fd and what it takes can be found here
@@ -5,10 +23,31 @@ function fbn --argument-names 'filename' 'path' -d "find by name. Looks for a fi
 	#
 	# fd can be installed with homebrew
 	# brew install fd
-	if test "$path" = ""
-		fd "$filename"
+	#
+	argparse 'exec=' -- $argv
+
+	if test "$path" = "" || test "--exec" = (echo "$path" | sd "(--exec)=.*" '$1')
+		if set -q _flag_exec
+			for file in (fd "$filename")
+				fbn_exec "$file" "$_flag_exec"
+			end
+		else
+			fd "$filename"
+		end
 	else
-		fd "$filename" $path
+		if set -q _flag_exec
+			for file in (fd "$filename" "$path")
+				fbn_exec "$file" "$_flag_exec"
+			end
+		else
+			fd "$filename" "$path"
+		end
 	end
+end
+
+function fbn_exec --argument-names 'file' 'exec'
+	echo "FILE: $file --------------------------------------"
+	set -l new_command (echo "$exec" | sed -e "s;{};$file;g")
+	eval "$new_command"
 end
 
