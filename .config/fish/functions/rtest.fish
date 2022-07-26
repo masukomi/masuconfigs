5c632f64a860dd4e82adf0962fd1b511cc089759
masukomi <masukomi@masukomi.org>
2022-07-26 10:39:56 -0400

    [fish] added rtest related convenience functions
    rtrs - rtestiy (rt) last rspec (rs) command
    rsrt - rspecify (rs) last rtest (rt) command

---
+++ .config/fish/functions/rtest.fish
@@ -0,0 +1,28 @@
+# some helper function to go back and forth between rtest and rspec
+# see https://github.com/masukomi/rtest
+
+function rtrs --description "rtestify (rt) last rspec (rs) command"
+	set -l command (
+		history \
+		| grep "^bundle exec rspec" \
+		| grep -v "rtest" \
+		| head -n 1 \
+		| sd "^.*rspec (-+\w+(=| )[0-9a-zA-Z-]+)* *" "rtest "
+		)
+
+	echo "running: $command"
+	eval $command
+end
+
+function rsrt --description "rspecify (rs) last rtest (rt) command"
+	set -l command (
+		history \
+		| grep "rtest " \
+		| grep -v "\--rspec" \
+		| head -n1\
+		| sd "rtest" "rtest --rspec" )
+	set -l c2 (eval $command)
+	echo "running: $c2"
+	eval $c2
+end
+
