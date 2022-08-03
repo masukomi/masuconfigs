fa7918add0fae1d7cdeed2ab77a2842e0bea5a11
masukomi <masukomi@masukomi.org>
2022-08-03 10:14:26 -0400

    fix(fish): brewed function used bashism and broke
    it was getting the first argument with $1 instead
    of getting it out of argv or making it a named
    parameter
    
    i went with named parameter
    
    TAGS:  library

--- .config/fish/functions/brewed.fish
+++ .config/fish/functions/brewed.fish
@@ -1,5 +1,5 @@
-function brewed
-	brew ls --versions $1 > /dev/null
+function brewed --argument-names package
+	brew ls --versions $package > /dev/null
 	if test $status -eq 0
 		# the package is installed
 		echo "installed"
