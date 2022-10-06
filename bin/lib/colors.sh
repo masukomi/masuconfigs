109e928679fa16e7d8f2340eeb9f941ea3627a80
masukomi <masukomi@masukomi.org>
2022-10-06 10:10:32 -0400

    [bash] add: ~/bin/lib/colors.sh common colors
    colors.sh contains some common bash color codes &
    emoji i use in scripts. made sense to finally
    break them out.
    
    acknowledging that no-one else is using this
    stuff means I don't have to worry about having
    scripts be self-contained and can do better reuse
    for myself, since... again, it only needs to work
    for me.
    
    TAGS: utilities

---
+++ bin/lib/colors.sh
@@ -0,0 +1,9 @@
+# A collection of common color stuff i use.
+
+ESCAPE=$(printf "\033")
+RED="${ESCAPE}[91m"
+GREEN="${ESCAPE}[92m"
+NOCOLOR="${ESCAPE}[0m"
+GREEN_PLUS_CHECK=$GREEN"✅"
+RED_PLUS_X=$RED"❌"
+
