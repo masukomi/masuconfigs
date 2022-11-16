bce1c68fd62b6def2ac28c435c97924fd0fea4b0
masukomi <masukomi@masukomi.org>
2022-11-16 17:36:56 -0500

    masuconfig_link no longer includes trailing newline

--- .config/fish/functions/masuconfig_link.fish
+++ .config/fish/functions/masuconfig_link.fish
@@ -10,7 +10,7 @@ function masuconfig_link --argument-names thing
 		set thing_path (echo $thing_path | sed -e "s/\/Users\/$me\///" )
 		set url "https://github.com/masukomi/masuconfigs/blob/master/$thing_path"
 		echo $url
-		echo $url | pbcopy
+		printf $url | pbcopy # printf instead of echo because we don't want the newline
 		echo "Copied to clipboard."
 	else
 		echo "Unable to find $thing in ~/bin or fish functions"
