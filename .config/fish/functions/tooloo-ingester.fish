35868207c032424941c91523206edbdbdf6f5fae
masukomi <masukomi@masukomi.org>
2022-11-21 16:59:41 -0500

    improved tooloo ingester

--- .config/fish/functions/tooloo-ingester.fish
+++ .config/fish/functions/tooloo-ingester.fish
@@ -1,14 +1,23 @@
 function tooloo-ingester
+  set -l all 0
   for file in (find . -name "*.meta.toml")
-      echo "include $file ?"
-      read -P "[n/q/enter] " yn
-      if test "$yn" = ""
-          tooloo add $file
-      else if test "$yn" = "q"
-          echo "quitting"
-          break
+      if test $all -eq 1
+        tooloo add $file
+        sleep 0.5
       else
-          echo "skipping..."
+        echo "include $file ?"
+        read -P "[n/a/q/enter] " yn
+        if test "$yn" = ""
+            tooloo add $file
+        else if test "$yn" = "q"
+            echo "quitting"
+            break
+        else if test "$yn" = "a"
+            set all 1
+            tooloo add $file
+        else
+            echo "skipping..."
+        end
       end
   end
 end
