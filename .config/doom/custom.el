304bc8f12dd22a4e02b148ee207f2bda67f28d9d
masukomi <masukomi@masukomi.org>
2025-09-28 22:39:09 -0400

    [org-mode] verse blocks colored and NEVER exported

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -1,18 +1,8 @@
-;;; custom.el -*- lexical-binding: t; -*-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(package-selected-packages '(org-hide-drawers))
- '(safe-local-variable-values '((display-line-numbers)))
- '(send-mail-function 'mailclient-send-it))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+; a stupid hack because I'll never normally use a "verse" block
+; but it's one of the few you can customize the styling for. So, I'm repurposing them.
+(with-eval-after-load 'org
+  (set-face-attribute 'org-verse nil
+                      :foreground "#c0abfe"
+                      :background "#333333"
+        )
+  )
