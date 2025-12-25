676bc4f29fa6770da35bfa86735e386b058d709e
masukomi <masukomi@masukomi.org>
2025-12-25 07:41:45 -0500

    [emacs] excluded some buffers from showing in tabs

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -250,6 +250,34 @@ current buffer's, reload dir-locals."
 ; group them by which projectile project we're in
 (centaur-tabs-group-by-projectile-project)
 
+(defun centaur-tabs-hide-tab (x)
+  "Do no to show buffer X in tabs."
+  (let ((name (format "%s" x)))
+    (or
+     ;; Current window is not dedicated window.
+     (window-dedicated-p (selected-window))
+
+     ;; Buffer name not match below blacklist.
+     (string-prefix-p "*epc" name)
+     (string-prefix-p "*helm" name)
+     (string-prefix-p "*Helm" name)
+     (string-prefix-p "*Compile-Log*" name)
+     (string-prefix-p "*lsp" name)
+     (string-prefix-p "*company" name)
+     (string-prefix-p "*Flycheck" name)
+     (string-prefix-p "*tramp" name)
+     (string-prefix-p " *Mini" name)
+     (string-prefix-p "*help" name)
+     (string-prefix-p "*straight" name)
+     (string-prefix-p " *temp" name)
+     (string-prefix-p "*Help" name)
+     (string-prefix-p "*mybuf" name)
+
+     ;; Is not magit buffer.
+     (and (string-prefix-p "magit" name)
+          (not (file-name-extension name)))
+     )))
+
 (global-set-key (kbd "s-w")  '+workspace/kill)
 
 ; Ctrl + =/+ contracts or expands visual selection
