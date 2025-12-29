e1bb7fe49d7cf7b4db5f8062c90ffdf933aa2dac
masukomi <masukomi@masukomi.org>
2025-12-29 18:30:32 -0500

    [doom] moving my doom config off into another repo

--- .config/doom/config.rb
+++
@@ -1,8 +0,0 @@
-def foo
-  puts "bar"
-end
-
-def bar
-  #here's a comment
-  puts "foo"
-end
--- .config/doom/custom.el
+++
@@ -1,8 +0,0 @@
-; a stupid hack because I'll never normally use a "verse" block
-; but it's one of the few you can customize the styling for. So, I'm repurposing them.
-(with-eval-after-load 'org
-  (set-face-attribute 'org-verse nil
-                      :foreground "#c0abfe"
-                      :background "#333333"
-        )
-  )
